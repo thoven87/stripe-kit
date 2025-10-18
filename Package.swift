2460fc4ebc8a59e10221b734c054503d5f2496ff
Stevenson Michel <130018170+thoven87@users.noreply.github.com>
2025-10-18 10:51:17 -0400

    Update async-http-client and swift-crypto dependencies
    Bump async-http-client to version 1.29.0 and adjust swift-crypto version range to 3.0.0..<5.0.0 in Package.swift for improved compatibility and access to newer features.

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     .library(name: "StripeKit", targets: ["StripeKit"])
   ],
   dependencies: [
-    .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.24.0"),
-    .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0"..<"4.0.0"),
+    .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.29.0"),
+    .package(url: "https://github.com/apple/swift-crypto.git", "3.0.0"..<"5.0.0"),
   ],
   targets: [
     .target(
